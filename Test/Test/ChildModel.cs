446e949b3f717e9aa6b38d306551bfcc09cbeb38
ddvk <ddvk>
2022-12-25 02:05:27 +0100

    initial NullRefexception

---
+++ Test/Test/ChildModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms.Internals;
+
+namespace Test
+{
+	public class ChildModel : INotifyPropertyChanged
+	{
+		public ChildModel()
+		{
+			Items = new ObservableCollection<int>();
+		}
+		public ObservableCollection<int> Items { get; set; }
+
+		private LayoutState _currentState = LayoutState.None;
+		public LayoutState CurrentState
+		{
+			get => this._currentState;
+			set => SetField(ref this._currentState, value);
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+		{
+			if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+			field = value;
+			OnPropertyChanged(propertyName);
+			return true;
+		}
+
+		public async Task Load()
+		{
+			CurrentState = LayoutState.Loading;
+			await Task.Delay(1000);
+			Enumerable.Range(0, 10).ForEach(x=> Items.Add(x));
+			CurrentState = LayoutState.None;
+		}
+	}
+}
---
+++ Test/Test/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			this.BindingContext = new MainViewModel();
+
+			InitializeComponent();
+		}
+
+		private async void TabViewEx_OnSelectionChanged(object sender, Syncfusion.XForms.TabView.SelectionChangedEventArgs e)
+		{
+			if (e.Index == 1)
+			{
+				await ((MainViewModel)this.BindingContext).ChildModel.Load();
+			}
+		}
+	}
+}
